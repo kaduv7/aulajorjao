45bd511eda326877c8682e6198a018592673cdff
kaduv7 <158072726+kaduv7@users.noreply.github.com>
2024-02-23 11:19:56 -0300

    Exercício 2

---
+++ Exercício 2/Exercício 2/Form1.cs
@@ -0,0 +1,25 @@
+namespace Exercício_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            retangulo r = new retangulo();
+            r.valor1 = Convert.ToInt32(textBox1.Text);
+            r.valor2 = Convert.ToInt32(textBox2.Text);
+            Int32 multiplicacao = r.valor1 * r.valor2;
+            MessageBox.Show("Resultado: " + multiplicacao.ToString());
+
+        }
+    }
+}
