c50735119ef83ed25651b10205b84d164b34ab89
kaduv7 <158072726+kaduv7@users.noreply.github.com>
2024-03-01 12:54:46 -0300

    exercício 7

---
+++ exercicio7/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,18 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            carro c = new carro();
+            c.marca = textBox1.Text;
+            c.modelo = textBox2.Text;
+            MessageBox.Show("Seu carro é da marca " + c.marca + ", modelo " + c.modelo);
+        }
+    }
+}
---
+++ exercicio7/WinFormsApp1/WinFormsApp1/carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class carro
+    {
+        public carro() { }
+
+        public string marca;
+        public string modelo;
+    }
+}
